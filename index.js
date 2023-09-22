08795f68a2e213fee4f8516cbd534d258f70d83f
Xhokabash <xhokabash@gmail.com>
2023-09-22 13:32:35 +0200

    adding multiple language greetings

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 const greetings = [
-    "Hi, I'm Xhoi.",
-    "Bonjour, je suis Xhoi.",
-    "Hola, soy Xhoi.",
-    "Ciao, sono Xhoi.",
-    "Kon'nichiwa, watashi wa Xhoi desu.",
-    "Hallo, ich bin Xhoi.",
-    "Përshëndetje, unë jam Xhoi." 
-]
+  `Hi, I'm <span class="hero">Xhoi</span>.`,
+  `Bonjour, je suis <span class="hero">Xhoi</span>.`,
+  `Hola, soy <span class="hero">Xhoi</span>.`,
+  `Ciao, sono <span class="hero">Xhoi</span>.`,
+  `Kon'nichiwa, watashi wa <span class="hero">Xhoi</span> desu.`,
+  `Hallo, ich bin <span class="hero">Xhoi</span>.`,
+  `Përshëndetje, unë jam <span class="hero">Xhoi</span>.`,
+];
 
+const greetingsElements = document.querySelector("#greeting");
 
-    const greetingsElements = document.querySelector("#greeting");
+function changeGreeting() {
+  const randomIndex = Math.floor(Math.random() * greetings.length);
+  greetingsElements.innerHTML = greetings[randomIndex];
+}
 
-    function changeGreeting(){
+changeGreeting();
 
-const randomIndex = Math.floor(Math.random() * greetings.length);
-greetingsElements.textContent = greetings[randomIndex];
-    }
-
-    changeGreeting();
-
-    setInterval(changeGreeting, 1000);
-
-
+setInterval(changeGreeting, 1000);
